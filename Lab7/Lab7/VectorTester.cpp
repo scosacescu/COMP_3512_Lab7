213ecb260f524e02f443bd9f718e7b78c89b063a
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-21 12:27:05 -0700

    simple handling of empty vectors being passed in

--- Lab7/Lab7/VectorTester.cpp
+++ Lab7/Lab7/VectorTester.cpp
@@ -7,15 +7,19 @@ int main() {
 	std::vector<int> ints2{ 5, 5, 5, 10, 6, 0 };
 	std::vector<int>empty;
 
+
 	vectorUtils::sum(ints);
+	vectorUtils::sum(ints2);
 	vectorUtils::max(ints);
 	vectorUtils::average(ints);
 	vectorUtils::min(ints);
+	
 	vectorUtils::max(empty);
+	vectorUtils::sum(empty);
+	vectorUtils::min(empty);
+	vectorUtils::average(empty);
 
-	//vu.sum(ints2);
-	//vu.max(ints2);
-	//vu.average(ints2);
+
 
 	cin.get();
 }
--- Lab7/Lab7/VectorUtils.hpp
+++ Lab7/Lab7/VectorUtils.hpp
@@ -11,12 +11,19 @@ namespace vectorUtils {
 
 
 	inline int sum(const std::vector<int>& v) {
-		int elementsum{ 0 };
-		for (auto& n : v) {
-			elementsum += n;
+		
+		if (!v.empty()) {
+			int elementsum = v[0];
+			for (std::vector<int>::const_iterator it = v.begin() + 1; it != v.end(); ++it) {
+				elementsum += *it;
+			}
+			std::cout << "the sum of your vector's elements is: " << elementsum << std::endl;
+			return elementsum;
+		} 
+		else {
+			std::cout << "Cannot find the sum of an empty vector" << std::endl;
+			return 0;
 		}
-		std::cout << "the sum of your vector's elements is: " << elementsum << std::endl;
-		return elementsum;
 	}
 
 	inline int max(const std::vector<int>& v) {
@@ -32,33 +39,46 @@ namespace vectorUtils {
 			return max;
 		}
 		else {
-			//std::cout << "Cannot find the max of an empty vector" << std::endl;
-			//return 0;
-			throw std::invalid_argument("cannot find the max of an empty vector");
+			std::cout << "Cannot find the max of an empty vector" << std::endl;
+			return 0;
+			//throw std::invalid_argument("cannot find the max of an empty vector");
 		}
 	}
 
 	inline int min(const std::vector<int>& v) {
-		int min = v[0];
 
-		for (std::vector<int>::const_iterator it = v.begin() + 1; it != v.end(); ++it) {
-			if (*it < min) {
-				min = *it;
+		if (!v.empty()) {
+			int min = v[0];
+			for (std::vector<int>::const_iterator it = v.begin() + 1; it != v.end(); ++it) {
+				if (*it < min) {
+					min = *it;
+				}
 			}
+			std::cout << "The smallest element in your vector is: " << min << std::endl;
+			return min;
 		}
-		std::cout << "The smallest element in your vector is: " << min << std::endl;
-		return min;
+		else {
+			std::cout << "Cannot find the min of an empty vector" << std::endl;
+			return 0;
+		}
 	}
 
 	inline double average(const std::vector<int>& v) {
-		double intermediateSum{ 0 };
-		double average{ 0.00 };
-		for (auto& n : v) {
-			intermediateSum += n;
+		if (!v.empty()) {
+			double intermediateSum{ 0 };
+			double average{ 0.00 };
+
+			for (auto& n : v) {
+				intermediateSum += n;
+			}
+			average = (intermediateSum / v.size());
+			std::cout << "The average value of your vector is: " << average << std::endl;
+			return average;
 		}
-		average = (intermediateSum / v.size());
-		std::cout << "The average value of your vector is: " << average << std::endl;
-		return average;
+		else{
+			std::cout << "Cannot find the average of an empty vector" << std::endl;
+			return 0;
+		}
 	}
 
 }
